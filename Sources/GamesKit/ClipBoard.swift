356474e30c7e3fcc1de78e56f5feb0d95b303cb8
Bastian Inuk Christensen <bastian@inuk.blog>
2019-12-21 16:34:03 +0100

    Fixed an issue where I called a non-static function in a static context

--- Sources/GamesKit/ClipBoard.swift
+++ Sources/GamesKit/ClipBoard.swift
@@ -1,9 +1,9 @@
 import CSDL2
 
 private struct ClipBoard {
-    static var value: String? { return try? getClipBoard()}
+    static var value: String? { return getClipBoard()}
     
-    private func getClipBoard() throws -> String {
+    static private func getClipBoard()  -> String {
         let pointer = SDL_GetClipboardText()!
         let safeClipBoard = String(cString: pointer)
         return safeClipBoard
